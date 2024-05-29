cfa9569de122eda533f5d394ae9d394800031e2e
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2024-05-29 04:30:39 -0300

    [feat] Add class Cliente and PessoaFisica

---
+++ main.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractclassmethod, abstractproperty
+from datetime import datetime
+
+class Cliente:
+    def __init__(self, endereco):
+        self._endereco = endereco
+        self.contas = []
+        
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+        
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+        
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
