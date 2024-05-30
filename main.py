4423b4216d66c75da658ed343dc8b76663ad4b97
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2024-05-29 23:32:14 -0300

    [feat] Add class Saque

--- main.py
+++ main.py
@@ -134,3 +134,17 @@ class Transacao(ABC):
     def registrar(self, conta):
         pass
         
+
+class Saque:
+    def __init__(self, valor):
+        self._valor = valor
+        
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+        
+        if sucesso_transacao:
+            conta.historico.adicionar_transao(self)
