32b904e380fb676d6645d2dcc49ebb1640fbd6dc
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2024-05-29 23:14:33 -0300

    [feat] Add class ContaCorrente

--- main.py
+++ main.py
@@ -77,6 +77,37 @@ class Conta:
             return False
         
         return True
-    
+
+class ContaCorrente(Conta):
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saques
         
         
+    def sacar(self, valor):
+        numero_saques = len([transacao for transacao in self.historico.transacoes if transacao["tipo" == Saque.__name__]])
+        
+        excedeu_limite = valor > self.limite
+        excedeu_saques = numero_saques >= self.limite_saques
+        
+        if excedeu_limite:
+            print("\nOperação não realizada! Valor excede o limite.")
+        elif excedeu_saques:
+            print("\nOperação não realizada! Número de saques excedeu o limite")
+        else:
+            return super().sacar(valor)
+            
+        return False
+        
+    def __str__(self):
+        return f"""\
+            Agência: \t{self.agencia}    
+            C/C:\t\t{self.numero}
+            Titular:\t{self.cliente.nome}
+        """
+        
+        
+# class Historico:
+#     def __init__(self):
+#         self._transacoes = []
